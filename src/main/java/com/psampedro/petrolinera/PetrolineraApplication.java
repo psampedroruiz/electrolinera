93e1a8997f17bfc2ca04e5838712aeb69b7f0f31
psampedroruiz <pablosampedroruiz@gmail.com>
2026-04-19 16:44:02 +0200

    Start Project

---
+++ src/main/java/com/psampedro/petrolinera/PetrolineraApplication.java
@@ -0,0 +1,13 @@
+package com.psampedro.petrolinera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetrolineraApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetrolineraApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/psampedro/petrolinera/PetrolineraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.psampedro.petrolinera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetrolineraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
